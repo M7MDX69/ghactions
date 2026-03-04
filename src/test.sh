c5c1a68b75acbddb1d58eb637893a70f02597985
MohammedAbdelrahman <Mohammedheliel6969@gmail.com>
2026-03-04 03:44:25 +0300

    New node app

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 EXPECTED="Hello, Test!"
 
-OUTPUT = $(node -e "console.log(require('./src/app)('Test'))")
+OUTPUT = $(node -e "console.log(require('./src/app')('Test'))")
 if [ "$OUTPUT" == "$EXPECTED" ]; then
 echo "Test Passed!"
 exit 0
